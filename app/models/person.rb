3c218f987e9ef337d81b16c70c7d61ba67033e76
Ben Chao <bchao@cs.washington.edu>
2017-06-09 22:49:21 -0700

    wrote some tests

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,8 @@
 class Person < ApplicationRecord
 
+    validates :height, presence: true
+    validates :preference, presence: true
+
     # limit options to cat and dogs
     enum preference: [:cat, :dog]
 
